488361be7de30b531913a233285ab6adac883c7c
chengchen886 <charlie@ccs.neu.edu>
2016-10-31 15:39:59 +0800

    web
    web

---
+++ Ads/src/main/webapp/js/AdsSearchController.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ChenCheng on 10/22/2016.
+ */
+
+angular.module("AdsSearchApp").controller('AdsSearchController', function ($scope, AdsSearchService, $window) {
+
+    $scope.searchText = null;
+    $scope.showResult = false;
+    $scope.tokens = [];
+
+    
+})
--- Ads/src/main/webapp/js/app.js
+++ Ads/src/main/webapp/js/app.js
@@ -1,4 +1,5 @@
 /**
  * Created by ChenCheng on 10/11/2016.
  */
-angular.module('AdsSearchApp', [])
+angular.module('AdsSearchApp', []);
+
